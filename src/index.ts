5639ebeb577c801f577c8e3e8a4b10af13312a4d
Miras <miamib531@gmail.com>
2023-07-07 17:28:55 +0600

    feat: Value of port on which application is running should be stored in .env file

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('heoolo hawau')
+import dotenv from 'dotenv'
+import http from 'http'
+dotenv.config()
+
+const PORT = process.env.PORT
+
+const server = http.createServer((req, res) => {})
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port http://localhost:${PORT}`)
+})
