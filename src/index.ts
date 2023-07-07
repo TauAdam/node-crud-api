9ac404eef06a4410a3d2b943f3b22dc598fe4b51
Miras <miamib531@gmail.com>
2023-07-07 17:46:10 +0600

    feat: GET api/users implemented properly

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
 import dotenv from 'dotenv'
 import http from 'http'
+import { User } from './models/index.js'
 dotenv.config()
 
 const PORT = process.env.PORT
 
-const server = http.createServer((req, res) => {})
+const users: User[] = []
+
+const server = http.createServer((req, res) => {
+  if (req.method === 'GET' && req.url === 'w') {
+    res.writeHead(200, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify(users))
+  } else {
+    res.statusCode = 404
+    res.end()
+  }
+})
 
 server.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`)
